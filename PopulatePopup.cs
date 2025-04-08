ed55d13765d375e110db9b085beb2d5c1f5bbeda
jakesjordan00 <119546212+jakesjordan00@users.noreply.github.com>
2025-04-08 10:59:25 -0400

    tooltips

--- PopulatePopup.cs
+++ PopulatePopup.cs
@@ -30,7 +30,11 @@ namespace NBAdbToolbox
             top += spacing + 20;
             CheckBox chk2 = new CheckBox() { Text = "Historic", Left = 20, Top = top, AutoSize = true };
             CheckBox chk3 = new CheckBox() { Text = "Current", Left = chk2.Right, Top = top, AutoSize = true };
-            top += spacing + 20;
+            top += spacing + 20; 
+            ToolTip tip = new ToolTip();
+            tip.SetToolTip(chk2, "Data gathered from NBA game page. Valid source for all seasons.\nHas more data than current source, but is of lesser quality.");
+            tip.SetToolTip(chk3, "Data gathered from NBA endpoints. Valid for 2019-2024. 2019 seems to be missing a few arbitrary games.");
+            tip.IsBalloon = true; // Rounded bubble style
 
             Button btnOK = new Button() { Text = "OK", Left = 80, Top = top, Width = 60, DialogResult = DialogResult.OK };
             Button btnCancel = new Button() { Text = "Cancel", Left = 150, Top = top, Width = 70, DialogResult = DialogResult.Cancel };
