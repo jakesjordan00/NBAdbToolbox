68df140dbab59fad38114bb768cccf8fc6260c69
JJ <jakesjordan00@gmail.com>
2025-04-15 01:05:11 -0400

    one more

--- Content/SQL/testing - quick deletes.sql
+++ Content/SQL/testing - quick deletes.sql
@@ -1,7 +1,7 @@
 
 
 select * from PlayByPlay p order by SeasonID desc, GameID desc, ActionNumber desc
-select * from StartingLineups
+select * from StartingLineups order by SeasonID desc, GameID desc
 select * from PlayerBox p order by SeasonID desc, GameID desc
 select * from TeamBox p order by SeasonID desc, GameID desc
 select * from TeamBoxLineups p order by SeasonID desc, GameID desc
@@ -11,16 +11,28 @@ select * from Game p order by SeasonID desc, GameID
 select * from Arena p order by SeasonID desc
 select * from Official p order by SeasonID desc
 
+select distinct SeasonID from Team
 
 select * from buildlog
 
-
+delete from StartingLineups
+delete from TeamBoxLineups
 delete from PlayByPlay
 delete from PlayerBox
 delete from TeamBox
-delete from TeamBoxLineups
-delete from Player
-delete from Team
-delete from Arena 
 delete from Game 
+delete from Player
 delete from Official 
+delete from Arena 
+delete from Team
+
+
+
+SELECT t.Name, p.rows Rows
+from sys.tables t inner join
+		sys.partitions p on t.object_id = p.object_id
+WHERE type_desc = 'USER_TABLE'
+
+
+
+select seasonID, Games + PlayoffGames Games from Season
